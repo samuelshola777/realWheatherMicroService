97cb5a688fa6eeaa58488e870e0ed1e53241fd9d
samuelshola777 <samuelshola14@gmail.com>
2023-03-17 14:29:53 +0100

    spring-boot weather micro service

--- src/main/java/com/example/realwheathermicroservice/controller/weatherController.java
+++ src/main/java/com/example/realwheathermicroservice/controller/weatherController.java
@@ -1,2 +1,14 @@
-package com.example.realwheathermicroservice.controller;public class weatherController {
+package com.example.realwheathermicroservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class weatherController {
+@GetMapping("/test")
+public String test(){
+   return "microService update weather MY NAME IS SAMUEL SHOLA";
+}
+
+
 }
