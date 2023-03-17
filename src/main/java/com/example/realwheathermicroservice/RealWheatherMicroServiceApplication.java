efea38772c385293cd449bf20f386f7d8770c1b0
samuelshola777 <samuelshola14@gmail.com>
2023-03-17 14:28:41 +0100

    Initial commit

---
+++ src/main/java/com/example/realwheathermicroservice/RealWheatherMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.realwheathermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWheatherMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWheatherMicroServiceApplication.class, args);
+	}
+
+}
