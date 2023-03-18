db242a9c29595d4d03191392e28d74f2e4ec8038
samuelshola777 <samuelshola14@gmail.com>
2023-03-18 13:11:26 +0100

    spring-boot weather micro service

--- src/main/java/com/example/realwheathermicroservice/controller/weatherController.java
+++ src/main/java/com/example/realwheathermicroservice/controller/weatherController.java
@@ -1,7 +1,10 @@
 package com.example.realwheathermicroservice.controller;
 
+import com.example.realwheathermicroservice.model.response.WeatherResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class weatherController {
@@ -9,6 +12,10 @@ public class weatherController {
 public String test(){
    return "microService update weather MY NAME IS SAMUEL SHOLA";
 }
-
+@GetMapping("/getWeather")
+public WeatherResponse getWeather(){
+    ResponseEntity<WeatherResponse> response = new RestTemplate().getForEntity("https://api.openweathermap.org/data/2.5/weather?lat=6.52319&lon=3.38044&appid=20f0506e31e420c5bc9889700b4310aa", WeatherResponse.class);
+return response.getBody();
+}
 
 }
--- src/main/java/com/example/realwheathermicroservice/model/response/WeatherResponse.java
+++ src/main/java/com/example/realwheathermicroservice/model/response/WeatherResponse.java
@@ -1,9 +1,14 @@
 package com.example.realwheathermicroservice.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class WeatherResponse {
 
     private Coordinates coordinates;
-    private Weather weather;
+    private Weather[] weather;
     private Mains main;
 
 }
